11df441d195da17f4e80de7b1f7676115e56c996
amahdavid <amahdavid34@gmail.com>
2024-05-07 21:48:08 -0700

    Added some files to be implemented later

--- app/index.tsx
+++ app/index.tsx
@@ -5,7 +5,7 @@ import { Link } from "expo-router";
 export default function App() {
   return (
     <View className= "flex-1 items-center justify-center bg-white">
-      <Text className = "text-3xl">Mobile App Dev!</Text>
+      <Text className = "text-3xl font-pblack">Mobile App Dev!</Text>
       <StatusBar style="auto" />
       <Link href="profile" style={{color: 'blue'}}>
         <Text>Go To Profile</Text>
