257054ced87cf03dc3f97d83db67fe528168ea84
amahdavid <amahdavid34@gmail.com>
2024-06-03 20:05:35 -0700

    Fixed dependcy issues, working on test

--- babel.config.js
+++ babel.config.js
@@ -3,17 +3,11 @@ module.exports = function (api) {
   return {
     presets: [
       'babel-preset-expo',
-      '@babel/preset-env',
-      '@babel/preset-react',
       '@babel/preset-typescript',
-      'module:metro-react-native-babel-preset'
     ],
     plugins: [
-      'nativewind/babel',
-      '@babel/plugin-transform-runtime',
-      ['@babel/plugin-proposal-class-properties', { loose: true }],
-      ['@babel/plugin-proposal-private-methods', { loose: true }],
-      ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
+      "nativewind/babel", 
+      "react-native-reanimated/plugin"
     ],
   };
 };
